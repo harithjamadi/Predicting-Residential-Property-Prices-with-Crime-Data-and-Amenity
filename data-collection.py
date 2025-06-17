3f593d628751076688683f71f054ece802a7d996
Abdullah Harith Jamadi <ajamadi@isimarkets.com>
2025-06-17 09:16:34 +0800

    start datacleaning

--- data-collection.py
+++ data-collection.py
@@ -3,7 +3,9 @@ import csv
 
 crime_data = pd.read_csv("data_source/crime_district.csv")
 
-transaction_data = pd.read_csv("data_source/Open Transaction Data.csv")
+transaction_data = pd.read_excel("data_source/Open Transaction Data.xlsx")
+
+transaction_data.fillna(method='ffill', inplace=True)
 
 print(transaction_data.head())
 print(crime_data.head())
